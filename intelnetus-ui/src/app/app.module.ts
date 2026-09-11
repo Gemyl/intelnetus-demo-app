7013b6ac32548c93045e5e8740579b221113a480
George <geomilo1998@gmail.com>
2026-09-11 13:48:02 +0300

    restores animations provider

--- intelnetus-ui/src/app/app.module.ts
+++ intelnetus-ui/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -28,6 +27,7 @@ import Aura from "@primeng/themes/aura";
     ToastsModule
 ],
   providers: [
+    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura
